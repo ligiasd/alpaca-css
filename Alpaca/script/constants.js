f244e89547986358811d6966982dbf4e550e3412
Ligiane Alves Souza Domingos <ligiane.alves.souza@gmail.com>
2021-09-16 14:56:28 -0300

    update files

---
+++ Alpaca/script/constants.js
@@ -0,0 +1,267 @@
+const backgrounds = [
+    {
+      name: "blue50",
+      url: "Alpaca/assets/backgrounds/blue50.png",
+    },
+    {
+      name: "blue60",
+      url: "Alpaca/assets/backgrounds/blue60.png",
+    },
+    {
+      name: "blue70",
+      url: "Alpaca/assets/backgrounds/blue70.png",
+    },
+    {
+      name: "darkblue30",
+      url: "Alpaca/assets/backgrounds/darkblue30.png",
+    },
+    {
+      name: "darkblue50",
+      url: "Alpaca/assets/backgrounds/darkblue50.png",
+    },
+    {
+      name: "darkblue70",
+      url: "Alpaca/assets/backgrounds/darkblue70.png",
+    },
+    {
+      name: "default",
+      url: "Alpaca/assets/backgrounds/default.png",
+    },
+    {
+      name: "green50",
+      url: "Alpaca/assets/backgrounds/green50.png",
+    },
+    {
+      name: "green60",
+      url: "Alpaca/assets/backgrounds/green60.png",
+    },
+    {
+      name: "green70",
+      url: "Alpaca/assets/backgrounds/green70.png",
+    },
+    {
+      name: "grey40",
+      url: "Alpaca/assets/backgrounds/grey40.png",
+    },
+    {
+      name: "grey70",
+      url: "Alpaca/assets/backgrounds/green60.png",
+    },
+    {
+      name: "grey80",
+      url: "Alpaca/assets/backgrounds/grey80.png",
+    },
+    {
+      name: "red50",
+      url: "Alpaca/assets/backgrounds/red50.png",
+    },
+    {
+      name: "red60",
+      url: "Alpaca/assets/backgrounds/red60.png",
+    },
+    {
+      name: "red70",
+      url: "Alpaca/assets/backgrounds/red70.png",
+    },
+    {
+      name: "yellow50",
+      url: "Alpaca/assets/backgrounds/yellow50.png",
+    },
+    {
+      name: "yellow60",
+      url: "Alpaca/assets/backgrounds/yellow60.png",
+    },
+  ];
+  
+  const earsItem = [
+    {
+      name: "default",
+      url: "Alpaca/assets/ears/default.png",
+    },
+    {
+      name: "tilt-backward",
+      url: "Alpaca/assets/ears/tilt-backward.png",
+    },
+    {
+      name: "tilt-forward",
+      url: "Alpaca/assets/ears/tilt-forward.png",
+    },
+  ];
+  
+  const accesItem = [
+    {
+      name: "default",
+      url: "Alpaca/assets/accessories/default.png",
+    },
+    {
+      name: "earings",
+      url: "Alpaca/assets/accessories/earings.png",
+    },
+    {
+      name: "flower",
+      url: "Alpaca/assets/accessories/flower.png",
+    },
+    {
+      name: "glasses",
+      url: "Alpaca/assets/accessories/glasses.png",
+    },
+  ];
+  
+  const eyesItem = [
+    {
+      name: "default",
+      url: "Alpaca/assets/eyes/default.png",
+    },
+    {
+      name: "angry",
+      url: "Alpaca/assets/eyes/angry.png",
+    },
+    {
+      name: "naughty",
+      url: "Alpaca/assets/eyes/naughty.png",
+    },
+    {
+      name: "panda",
+      url: "Alpaca/assets/eyes/panda.png",
+    },
+    {
+      name: "smart",
+      url: "Alpaca/assets/eyes/smart.png",
+    },
+    {
+      name: "star",
+      url: "Alpaca/assets/eyes/star.png",
+    },
+  ];
+  
+  const hairItem = [
+    {
+      name: "default",
+      url: "Alpaca/assets/hair/default.png",
+    },
+    {
+      name: "bang",
+      url: "Alpaca/assets/hair/bang.png",
+    },
+    {
+      name: "curls",
+      url: "Alpaca/assets/hair/curls.png",
+    },
+    {
+      name: "elegant",
+      url: "Alpaca/assets/hair/elegant.png",
+    },
+    {
+      name: "fancy",
+      url: "Alpaca/assets/hair/fancy.png",
+    },
+    {
+      name: "quiff",
+      url: "Alpaca/assets/hair/quiff.png",
+    },
+    {
+      name: "short",
+      url: "Alpaca/assets/hair/short.png",
+    },
+  ];
+  
+  const legItem = [
+    {
+      name: "default",
+      url: "Alpaca/assets/leg/default.png",
+    },
+    {
+      name: "bubble-tea",
+      url: "Alpaca/assets/leg/bubble-tea.png",
+    },
+    {
+      name: "cookie",
+      url: "Alpaca/assets/leg/cookie.png",
+    },
+    {
+      name: "game-console",
+      url: "Alpaca/assets/leg/game-console.png",
+    },
+    {
+      name: "tilt-backward",
+      url: "Alpaca/assets/leg/tilt-backward.png",
+    },
+    {
+      name: "tilt-forward",
+      url: "Alpaca/assets/leg/tilt-forward.png",
+    },
+  ];
+  
+  const mouthItem = [
+    {
+      name: "default",
+      url: "Alpaca/assets/mouth/default.png",
+    },
+    {
+      name: "astonished",
+      url: "Alpaca/assets/mouth/astonished.png",
+    },
+    {
+      name: "eating",
+      url: "Alpaca/assets/mouth/eating.png",
+    },
+    {
+      name: "laugh",
+      url: "Alpaca/assets/mouth/laugh.png",
+    },
+    {
+      name: "tongue",
+      url: "Alpaca/assets/mouth/tongue.png",
+    },
+  ];
+  
+  const neckItem = [
+    {
+      name: "default",
+      url: "Alpaca/assets/neck/default.png",
+    },
+    {
+      name: "bend-backward",
+      url: "Alpaca/assets/neck/bend-backward.png",
+    },
+    {
+      name: "bend-forward",
+      url: "Alpaca/assets/neck/bend-forward.png",
+    },
+    {
+      name: "thick",
+      url: "Alpaca/assets/neck/thick.png",
+    },
+  ];
+  
+  const noseItem = [
+    {
+      name: "default",
+      url: "Alpaca/assets/nose/default.png",
+    },
+  ];
+  
+  const btnGeral = [
+    "backgrounds",
+    "ears",
+    "eyes",
+    "hair",
+    "leg",
+    "mouth",
+    "neck",
+    "nose",
+    "accessories",
+  ];
+  
+  export default {
+    btnGeral,
+    noseItem,
+    neckItem,
+    mouthItem,
+    legItem,
+    hairItem,
+    eyesItem,
+    accesItem,
+    earsItem,
+    backgrounds,
+  };
---
+++ Alpaca/script/index.js
@@ -0,0 +1,161 @@
+import("./constants").then((module) => {
+    const constants = module.default;
+  
+    function geraBtnGeral(btnGeral) {
+      btnGeral.forEach((item) => {
+        const button = document.getElementById(`id-${item}`);
+        button.addEventListener("click", exibiBtn);
+      });
+    }
+    geraBtnGeral(constants.btnGeral);
+  
+    function styleBtn(name, src) {
+      switch (name) {
+        case "backgrounds":
+          document.getElementById("imgBack").src = src;
+          break;
+        case "ears":
+          document.getElementById("imgEars").src = src;
+          break;
+        case "accessories":
+          document.getElementById("imgAcces").src = src;
+          break;
+        case "eyes":
+          document.getElementById("imgEyes").src = src;
+          break;
+        case "hair":
+          document.getElementById("imgHair").src = src;
+          break;
+        case "leg":
+          document.getElementById("imgLeg").src = src;
+          break;
+        case "mouth":
+          document.getElementById("imgMouth").src = src;
+          break;
+        case "neck":
+          document.getElementById("imgNeck").src = src;
+          break;
+        case "nose":
+          document.getElementById("imgNose").src = src;
+          break;
+        default:
+          break;
+      }
+    }
+  
+    function geraBtn(origin, items) {
+      const content = document.getElementById("contentBtn");
+  
+      while (content.firstChild) {
+        content.removeChild(content.firstChild);
+      }
+  
+      items.forEach((item) => {
+        const btn = document.createElement("button");
+        btn.classList.add("btn");
+        btn.setAttribute("name", `${item.name}`);
+        btn.setAttribute("id", `id-${item.name}`);
+        btn.innerText = `${item.name}`;
+        content.appendChild(btn);
+  
+        btn.addEventListener("click", () => styleBtn(origin, item.url));
+      });
+    }
+  
+    function exibiBtn(event) {
+      console.log(event);
+      switch (event.target.name) {
+        case "backgrounds":
+          geraBtn("backgrounds", constants.backgrounds);
+          break;
+        case "ears":
+          geraBtn("ears", constants.earsItem);
+          break;
+        case "eyes":
+          geraBtn("eyes", constants.eyesItem);
+          break;
+        case "hair":
+          geraBtn("hair", constants.hairItem);
+          break;
+        case "leg":
+          geraBtn("leg", constants.legItem);
+          break;
+        case "mouth":
+          geraBtn("mouth", constants.mouthItem);
+          break;
+        case "neck":
+          geraBtn("neck", constants.neckItem);
+          break;
+        case "nose":
+          geraBtn("nose", constants.noseItem);
+          break;
+        case "accessories":
+          geraBtn("accessories", constants.accesItem);
+          break;
+        default:
+          break;
+      }
+
+    }
+  
+    function salvaAvatar(event) {
+      const avatar = document.getElementById("avatar");
+      console.log(avatar);
+  
+      let imgBack = new Image();
+      imgBack.src = document.getElementById("imgBack").src;
+  
+      let imgEars = new Image();
+      imgEars.src = document.getElementById("imgEars").src;
+  
+      let imgNeck = new Image();
+      imgNeck.src = document.getElementById("imgNeck").src;
+  
+      let imgNose = new Image();
+      imgNose.src = document.getElementById("imgNose").src;
+  
+      let imgHair = new Image();
+      imgHair.src = document.getElementById("imgHair").src;
+  
+      let imgEyes = new Image();
+      imgEyes.src = document.getElementById("imgEyes").src;
+  
+      let imgMouth = new Image();
+      imgMouth.src = document.getElementById("imgMouth").src;
+  
+      let imgLeg = new Image();
+      imgLeg.src = document.getElementById("imgLeg").src;
+  
+      let imgAcces = new Image();
+      imgAcces.src = document.getElementById("imgAcces").src;
+  
+      const canvas = document.getElementById("canvas");
+      const ctx = canvas.getContext("2d");
+  
+      ctx.save();
+      ctx.drawImage(imgBack, 0, 0, 300, 300);
+      ctx.drawImage(imgEars, 0, 0, 300, 300);
+      ctx.drawImage(imgNeck, 0, 0, 300, 300);
+      ctx.drawImage(imgNose, 0, 0, 300, 300);
+      ctx.drawImage(imgHair, 0, 0, 300, 300);
+      ctx.drawImage(imgEyes, 0, 0, 300, 300);
+      ctx.drawImage(imgMouth, 0, 0, 300, 300);
+      ctx.drawImage(imgLeg, 0, 0, 300, 300);
+      ctx.drawImage(imgAcces, 0, 0, 300, 300);
+      ctx.restore();
+  
+      const reader = new FileReader();
+      reader.onload = () => {
+         const image = canvas.toDataURL("image/png")
+         console.log(image);
+         canvas.toBlod(blob => {
+              saveAs(blob, "image.png")
+         })
+      }
+       
+      
+    }
+  
+    const salvar = document.getElementById(`btn-download`);
+    salvar.addEventListener("click", salvaAvatar);
+  });
